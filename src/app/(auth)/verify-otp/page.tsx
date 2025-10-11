edfd1647d1c01bcaba047b21955a85dc80006c0a
Aegon017 <kondanagamalleswararao016@gmail.com>
2025-10-11 11:09:39 +0530

    changed otp verification page styling

--- src/app/(auth)/verify-otp/page.tsx
+++ src/app/(auth)/verify-otp/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import { Suspense } from "react";
-// import { VerifyOtpForm } from "@/components/verify-otp-form";
+import { VerifyOtpForm } from "@/components/verify-otp-form";
 
 export default function VerifyOtpPage() {
   return (
     <Suspense fallback={ <p>Loading...</p> }>
       <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
         <div className="w-full max-w-sm md:max-w-3xl">
-          sdafasfda
+          <VerifyOtpForm />
         </div>
       </div>
     </Suspense >
