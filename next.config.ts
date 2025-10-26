9792aa58f21f179687b0edb2749b7d26092e2559
Aegon017 <kondanagamalleswararao016@gmail.com>
2025-10-26 19:18:46 +0530

    removed local images in next config

--- next.config.ts
+++ next.config.ts
@@ -5,24 +5,8 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "http",
-        hostname: "127.0.0.1",
-      },
-      {
-        protocol: "http",
         hostname: "localhost",
         port: "8000",
-      },
-      {
-        protocol: "http",
-        hostname: "192.168.1.2",
-      },
-      {
-        protocol: "http",
-        hostname: "192.168.1.42",
-      },
-      {
-        protocol: "http",
-        hostname: "192.168.0.105",
       },
       {
         protocol: "https",
