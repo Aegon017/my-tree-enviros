72e960a8a349ae313ecf688e0ea330dd12eda7fe
Aegon017 <kondanagamalleswararao016@gmail.com>
2025-11-13 10:36:22 +0530

    changed product design

--- src/components/skeletons/product-card-skeleton.tsx
+++ src/components/skeletons/product-card-skeleton.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from "../ui/skeleton";
 
 const ProductCardSkeleton = () => (
   <Card className="py-0 w-full">
-    <Skeleton className="h-36 w-full" />
+    <Skeleton className="h-60 w-full" />
     <CardContent className="p-4 space-y-3">
       <Skeleton className="h-3 w-20" />
       <Skeleton className="h-4 w-32" />
