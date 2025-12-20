d230a05df7d34e1b2c0050252208961186b73978
Aegon017 <kondanagamalleswararao016@gmail.com>
2025-12-20 17:30:25 +0530

    feat: add application header with navigation and user features, and implement Firebase messaging service worker

--- src/components/ui/app-logo.tsx
+++ src/components/ui/app-logo.tsx
@@ -2,7 +2,6 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { useIsMobile } from "@/hooks/use-mobile";
 import logo from "../../../public/web-logo.png";
 import { cn } from "@/lib/utils";
 
@@ -11,16 +10,12 @@ interface Props {
 }
 
 const AppLogo = ({ className }: Props) => {
-  const isMobile = useIsMobile();
-
   return (
     <Link href="/" className="shrink-0 mx-2 sm:mx-4">
       <Image
         src={logo}
-        width={isMobile ? 240 : 240}
-        height={isMobile ? 240 : 240}
         alt="My Tree Enviros Logo"
-        className={cn("object-contain", className)}
+        className={cn("object-contain mt-4 md:mt-0", className)}
         priority
       />
     </Link>
